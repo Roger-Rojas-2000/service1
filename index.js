35c072d0fe04997531edad8584d63f9915aa5b08
Roger Rojas <roger.rojas189@gmail.com>
2025-08-01 22:46:07 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hola desde el index.')
+})
+
+app.listen(3000, ()=>{
+  console.log('Listening on port 3000')
+})
+
