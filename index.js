48ed338b4ed0e1c61f1aa575b64dc368751f9c36
Roger Rojas <roger.rojas189@gmail.com>
2025-08-04 22:06:45 -0400

    Test of trigger

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from 'express'
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hola desde el index.')
+  res.send('Hola desde el index. Corriendo con trigger')
 })
 
 app.listen(3000, ()=>{
